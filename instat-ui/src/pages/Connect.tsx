5ce26ce2367e103139c3ce8a77f5afcc18208f90
BoyWithUke <117911858+Boy-With-Uke@users.noreply.github.com>
2024-05-16 20:57:14 +0300

    connexion mety

--- instat-ui/src/pages/Connect.tsx
+++ instat-ui/src/pages/Connect.tsx
@@ -1,14 +1,15 @@
-import { dividerClasses } from "@mui/material";
+import { Link } from "react-router-dom";
 import Button from "react-bootstrap/Button";
 import Col from "react-bootstrap/Col";
 import Container from "react-bootstrap/Container";
-import Modal from "react-bootstrap/Modal";
+import "../assets/auth.css";
 import Row from "react-bootstrap/Row";
 import Form from "react-bootstrap/Form";
 import Swal from "sweetalert2";
 import Cookies from "js-cookie";
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
+import img from "../assets/Images/instat_logo2.jpg";
 export default function Connect() {
   const [mail, setMail] = useState("");
   const [password, setPassword] = useState("");
@@ -23,8 +24,8 @@ export default function Connect() {
     if (data.result === null) {
       Swal.fire({
         icon: "error",
-        title: "Utilisateur non trouves",
-        text: "Reverifier vos identifiants",
+        title: "Utilisateur non trouvés",
+        text: "Reverifiez vos identifiants",
       });
     } else {
       const idUser = data.result;
@@ -35,27 +36,57 @@ export default function Connect() {
   return (
     <>
       <div className="container-fluid bg-dark bg-gradient authSection">
-        <Container>
-          <Form>
+        <div className="outter">
+          <Container>
             <Row>
-              <Form.Label>Email address</Form.Label>
-              <Form.Control
-                type="email"
-                value={mail}
-                onChange={(event) => setMail(event.target.value)}
-              />
+              <Col md={6} className="login-image">
+                <img src={img} alt="Login" />
+              </Col>
+              <Col md={6}>
+                <div className="FormWarper" style={{ textAlign: "center" }}>
+                  <Form>
+                    <Form.Label className="title">Connexion</Form.Label>
+                    <Row className="rows" style={{ marginTop: "40px" }}>
+                      <Form.Group controlId="formBasicEmail">
+                        <Form.Control
+                          type="email"
+                          value={mail}
+                          onChange={(event) => setMail(event.target.value)}
+                          placeholder="Enter email"
+                        />
+                      </Form.Group>
+                    </Row>
+                    <Row className="rows" style={{ marginTop: "40px" }}>
+                      <Form.Group controlId="formBasicPassword">
+                        <Form.Control
+                          type="password"
+                          value={password}
+                          onChange={(event) => setPassword(event.target.value)}
+                          placeholder="Password"
+                        />
+                      </Form.Group>
+                    </Row>
+                    <Row className="rows" style={{ marginTop: "40px" }}>
+                      <Button
+                        variant="primary"
+                        onClick={handleConnect}
+                        style={{
+                          backgroundColor: "#003529",
+                          border: "#003529",
+                        }}
+                      >
+                        Connexion
+                      </Button>
+                    </Row>
+                    <Row style={{marginTop: '20px'}}>
+                      <Link to={"/signin"} className="custom-Link">Pas encore de compte?</Link>
+                    </Row>
+                  </Form>
+                </div>
+              </Col>
             </Row>
-            <Row>
-              <Form.Label>Password</Form.Label>
-              <Form.Control
-                type="password"
-                value={password}
-                onChange={(event) => setPassword(event.target.value)}
-              />
-            </Row>
-            <Button onClick={handleConnect}>Connexion</Button>
-          </Form>
-        </Container>
+          </Container>
+        </div>
       </div>
     </>
   );
